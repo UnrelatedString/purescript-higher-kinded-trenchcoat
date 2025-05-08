3f1004ac0b2c08fb200295ac2142a0903d3994c6
UnrelatedString <unrelatedstring@gmail.com>
2025-05-08 13:26:24 -0400

    hungryyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyy canbt tghink

--- src/Type/Trenchcoat.purs
+++ src/Type/Trenchcoat.purs
@@ -1,6 +1,7 @@
 module Type.Trenchcoat
   ( Trenchcoat
   , class PseudoFunctor
+  , class PartialFunctor
   , pseudoMap
   , disguise
   , undisguise
@@ -9,8 +10,11 @@ module Type.Trenchcoat
 
 import Prelude
 
+import Data.Const (Const(..))
+import Data.Newtype (un)
 import Data.Set as Set
 import Data.String.CodePoints (CodePoint, toCodePointArray, fromCodePointArray)
+import Type.Prelude (class TypeEquals)
 
 -- okay so I just now realized it'll probably take some like super wacky
 -- row magic to make a single Trenchcoat value capable of adapting multiple classes
@@ -18,8 +22,10 @@ import Data.String.CodePoints (CodePoint, toCodePointArray, fromCodePointArray)
 -- literally just. be stored inside a record. and use another class to map 
 -- ((Type -> Type) -> Constraint)s to Symbols I guess idk)
 -- but anyways I'm just going to start with functors as a proof of concept before I get
--- lost in the weeds on that lmao. and just for the String-like case, then think about
--- how to do constrained actual HKTs like Set later
+-- lost in the weeds on that lmao.
+-- and turns out the constraint thing actually is pretty easy to roll in 
+-- ...... except do I really want to fuse it like this or do I want to think of a more
+-- generic way to reuse the machinery and ughhhhhhhhhhhhhhhhhhhhh
 
 -- also really torn about how or if to do stuff like Apply where it's like,
 -- a Set literally can't contain functions but I can still see it being useful to
@@ -32,13 +38,15 @@ import Data.String.CodePoints (CodePoint, toCodePointArray, fromCodePointArray)
 -- ...or for Unfoldable since that fundamentally doesn't have disguise
 -- ........
 -- yeah good thing I started small jgf;aojg ;erg ;
+-- and what even other classes are there to support? Bifunctor?? maybe
 
 data Trenchcoat :: Type -> Type -> Type -> Type
 data Trenchcoat f a b = Trenchcoat f (a -> b)
 
-class PseudoFunctor :: Type -> Type -> Constraint
-class PseudoFunctor f a where
-  pseudoMap :: (a -> a) -> f -> f
+type PseudoFunctor :: Type -> Type -> Constraint
+type PseudoFunctor f a = PartialFunctor f (TypeEquals a)
+
+class PartialFunctor :: 
 
 disguise :: forall f a. f -> Trenchcoat f a a
 disguise = flip Trenchcoat identity
